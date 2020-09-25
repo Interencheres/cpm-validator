0707856e31c9d910f0c526aa998e066266c25ce1
namelot <namelot@cpmultimedia.com>
2020-09-25 11:48:22 +0200

    test(schema): follow JSON Schema spec

--- tests/schemas/test.js
+++ tests/schemas/test.js
@@ -1,12 +1,11 @@
 "use strict";
 
 module.exports = {
-    required: true,
+    required: ["name", "status", "options"],
     type: "object",
     additionalProperties: false,
     properties: {
         name: {
-            required: true,
             type: "object",
             additionalProperties: false,
             anyOf: [
@@ -15,33 +14,27 @@ module.exports = {
             ],
             properties: {
                 first: {
-                    required: false,
                     type: "string"
                 },
                 second: {
-                    required: false,
                     type: "string"
                 }
             }
         },
         nonfloating: {
-            required: false,
             type: "integer"
         },
         floating: {
-            required: false,
             type: "number"
         },
         description: {
-            required: false,
             type: "string"
         },
         status: {
-            required: true,
             type: "object"
         },
         options: {
-            required: true,
+            required: ["name"],
             type: "object",
             additionalProperties: false,
             anyOf: [
@@ -49,7 +42,6 @@ module.exports = {
             ],
             properties: {
                 name: {
-                    required: true,
                     type: "object",
                     additionalProperties: false,
                     anyOf: [
@@ -58,11 +50,9 @@ module.exports = {
                     ],
                     properties: {
                         first: {
-                            required: false,
                             type: "string"
                         },
                         second: {
-                            required: false,
                             type: "string"
                         }
                     }
